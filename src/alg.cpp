e13353aad602d17d7b386f963d74037a078f18cd
TIMUR <timur.sinev2000@mail.ru>
2026-03-01 01:55:38 +0300

    b88.3

--- src/alg.cpp
+++ src/alg.cpp
@@ -7,8 +7,9 @@
 
 bool checkPrime(uint64_t value) {
   if (value < 2) return false;
-  if (value == 2  value == 3) return true;
-  if (value % 2 == 0  value % 3 == 0) return false;
+  if (value == 2 || value == 3) return true; // Тот самый фикс
+  if (value % 2 == 0 || value % 3 == 0) return false;
+
   for (uint64_t i = 5; i * i <= value; i += 6) {
     if (value % i == 0 || value % (i + 2) == 0)
       return false;
