8a9a00cf0291464863dbcff0cfe2e20afa64414c
TIMUR <timur.sinev2000@mail.ru>
2026-03-01 01:51:13 +0300

    b88.1

--- test/tests.cpp
+++ test/tests.cpp
@@ -5,13 +5,57 @@
 #include "alg.h"
 
 
-TEST(st1, sumPrime1) {
-  uint64_t res = sumPrime(2000000);
-  uint64_t expected = 142913828922;
-  EXPECT_EQ(expected, res);
+
+TEST(AlgTest, CheckPrimeBasic) {
+  EXPECT_TRUE(checkPrime(2));
+  EXPECT_TRUE(checkPrime(7));
+  EXPECT_TRUE(checkPrime(13));
+  EXPECT_FALSE(checkPrime(1));
+  EXPECT_FALSE(checkPrime(15));
 }
-TEST(st1, sumPrime2) {
-  uint64_t res = sumPrime(10);
-  uint64_t expected = 17;
-  EXPECT_EQ(expected, res);
+
+TEST(AlgTest, CheckPrimeZeroAndOne) {
+  EXPECT_FALSE(checkPrime(0));
+  EXPECT_FALSE(checkPrime(1));
 }
+
+TEST(AlgTest, CheckPrimeComposite) {
+  EXPECT_FALSE(checkPrime(4));
+  EXPECT_FALSE(checkPrime(9));
+}
+
+TEST(AlgTest, NPrimeBasic) {
+  EXPECT_EQ(nPrime(1), 2);
+  EXPECT_EQ(nPrime(2), 3);
+  EXPECT_EQ(nPrime(3), 5);
+  EXPECT_EQ(nPrime(10), 29);
+}
+
+TEST(AlgTest, NPrimeLarge) {
+  EXPECT_EQ(nPrime(500), 3571);
+}
+
+TEST(AlgTest, NextPrimeBasic) {
+  EXPECT_EQ(nextPrime(4), 5);
+  EXPECT_EQ(nextPrime(11), 13);
+  EXPECT_EQ(nextPrime(2), 3);
+}
+
+TEST(AlgTest, NextPrimeFromLarge) {
+  EXPECT_EQ(nextPrime(14), 17);
+}
+
+TEST(AlgTest, SumPrimeBasic) {
+  EXPECT_EQ(sumPrime(10), 17);
+}
+
+TEST(AlgTest, SumPrimeSmallBoundary) {
+  EXPECT_EQ(sumPrime(2), 0);
+  EXPECT_EQ(sumPrime(3), 2);
+}
+
+TEST(AlgTest, SumPrimeNoPrimes) {
+  EXPECT_EQ(sumPrime(0), 0);
+  EXPECT_EQ(sumPrime(1), 0);
+}
+
